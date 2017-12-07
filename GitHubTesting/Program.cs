c53a8a79f4b3b9b51e19ddc740acf5dc9a08f3ee
damiansurma <damian.surma@tlen.pl>
2017-12-08 00:46:29 +0100

    strategy design pattern test implementation

--- GitHubTesting/Program.cs
+++ GitHubTesting/Program.cs
@@ -4,15 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// strategy design pattern test
+
 namespace GitHubTesting
 {
-    interface DesignPatter
+    interface DefaultSolution
     {
         bool checkValue();
     }
 
 
-    class Program : DesignPatter
+    class Solution1 : DefaultSolution
+    {
+        public bool checkValue() { Console.WriteLine("true"); return true;  }
+    }
+    class Solution2 : DefaultSolution
+    {
+        public bool checkValue() { Console.WriteLine("false"); return false; }
+    }
+
+    class ValueCheckerPerson
+    {
+        DefaultSolution checker;
+
+        public void setChecker(DefaultSolution _checker)  {
+            checker = _checker;
+        }
+
+        public void doStuff()
+        {
+            checker.checkValue();
+        }
+
+    }
+
+    class Program
     {
         public bool checkValue() { return false; }
 
@@ -20,7 +47,19 @@ namespace GitHubTesting
 
         static void Main(string[] args)
         {
+            // just some changes
             Console.WriteLine("Hello World");
+
+            DefaultSolution sol1 = new Solution1();
+            DefaultSolution sol2 = new Solution2();
+
+            ValueCheckerPerson test = new ValueCheckerPerson();
+
+            test.setChecker(sol2);
+            test.doStuff();
+
+            test.setChecker(sol1);
+            test.doStuff();
         }
     }
 }

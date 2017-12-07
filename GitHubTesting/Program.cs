925557a36932518bbb59236e5dd6ef80f5996a56
damiansurma <damian.surma@tlen.pl>
2017-12-08 00:06:59 +0100

    Add project files.

---
+++ GitHubTesting/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTesting
+{
+    interface DesignPatter
+    {
+        void checkValue();
+    }
+
+
+    class Program : DesignPatter
+    {
+        public void checkValue() {  }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
